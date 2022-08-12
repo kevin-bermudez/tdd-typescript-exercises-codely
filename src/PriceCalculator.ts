4e6cb9183e201c93250262208f211f1d248e1107
kevin-bermudez <kevinbermudezmejia@gmail.com>
2022-08-11 23:41:35 -0500

    refactor: fix typescript eslintrc

--- src/PriceCalculator.ts
+++ src/PriceCalculator.ts
@@ -1,11 +1,11 @@
 export class PriceCalculator {
   subscriptionsNumber: number;
-  priceRanges = [
+  priceRanges: { low: number; high: number; price: number }[] = [
     { low: 1, high: 2, price: 299 },
     { low: 3, high: 10, price: 239 },
     { low: 11, high: 25, price: 219 },
     { low: 26, high: 50, price: 199 },
-    { low: 51, price: 149 },
+    { low: 51, high: 0, price: 149 },
   ];
 
   constructor(subscriptionsNumber: number) {
@@ -15,11 +15,13 @@ export class PriceCalculator {
   getTotalPrice(): number {
     const rangePrice = this.priceRanges.find(
       (range) =>
-        (!range.high && this.subscriptionsNumber >= range.low) ||
-        (this.subscriptionsNumber >= range.low &&
-          this.subscriptionsNumber <= range.high)
+        this.subscriptionsNumber >= range.low &&
+        this.subscriptionsNumber <= range.high
     );
 
-    return rangePrice?.price * this.subscriptionsNumber;
+    return rangePrice
+      ? rangePrice.price * this.subscriptionsNumber
+      : this.priceRanges[this.priceRanges.length - 1].price *
+          this.subscriptionsNumber;
   }
 }
