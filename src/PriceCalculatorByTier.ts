c40fb5f8669910b9ca2b89473d62612f4c5bf44f
kevin-bermudez <kevinbermudezmejia@gmail.com>
2022-08-12 00:39:09 -0500

    refactor: values for tiers in constants

--- src/PriceCalculatorByTier.ts
+++ src/PriceCalculatorByTier.ts
@@ -1,16 +1,24 @@
 export class PriceCalculatorByTier {
   subscriptions: number;
+  tierPrices = {
+    first: 299,
+    second: 239,
+    thirth: 219,
+  };
+
   constructor(subscriptions: number) {
     this.subscriptions = subscriptions;
   }
 
   getTotalPrice(): number {
+    const firstTier = this.tierPrices.first * 2;
+    const secondTier = this.tierPrices.second * 8;
     if (this.subscriptions == 3) {
-      return 299 * 2 + 239;
+      return firstTier + this.tierPrices.second;
     }
 
     if (this.subscriptions == 11) {
-      return 299 * 2 + 239 * 8 + 219;
+      return firstTier + secondTier + this.tierPrices.thirth;
     }
 
     return 299 * this.subscriptions;
